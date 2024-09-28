ad8d93d4a3c7834cbf0decbb0e2a1151c2b39aca
Gustavo Nascimento Martins <gustavonascimento889@gmail.com>
2024-09-28 12:36:35 -0300

    Update Desafio2.c

--- Desafio2.c
+++ Desafio2.c
@@ -5,90 +5,79 @@
 #ifndef REMOVE_H
 #define REMOVE_H
 #endif
-int func_val(int x, int b);
 
-//  retorna o valor arredondado 
-int func_val(int x, int b) {
-    double result = x / (double)b;
-    return round(result);
+// Função para ter o valor arredondado
+int calcular_valor(int x, int b) {
+    double resultado = (double)x / b;
+    return (int)round(resultado);
 }
-//  tabela com os limites das variaveis
-void print_limits() {
-	
-    printf("-------------------------------------------------------------------------\n");
-    printf("| Tipo                    | Minimo               | Maximo               |\n");
-    printf("-------------------------------------------------------------------------\n");
 
-    printf("| char                    | %20d | %20d |\n", CHAR_MIN, CHAR_MAX);
-    
-    printf("| int                     | %20d | %20d |\n", INT_MIN, INT_MAX);
-    
-    printf("| short int               | %20d | %20d |\n", SHRT_MIN, SHRT_MAX);
+// Tabela 
+void exibir_limites() {
 
-    printf("| unsigned int            | %20u | %20u |\n", 0, UINT_MAX);
-
-    printf("| long int                | %20ld | %20ld |\n", LONG_MIN, LONG_MAX);
-  
-    printf("| unsigned long int       | %20lu | %20lu |\n", 0UL, ULONG_MAX);
-    
-    printf("| long long int           | %20lld | %20lld |\n", LLONG_MIN, LLONG_MAX);
-
-    printf("| unsigned long long int  | %20llu | %20llu |\n", 0ULL, ULLONG_MAX);
-    
-    printf("-------------------------------------------------------------------------\n");
+    printf("| Tipo                   | Valor Minimo    | Valor Maximo |\n");
+    printf("| char                   | %20d | %20d |\n", CHAR_MIN, CHAR_MAX);
+    printf("| int                    | %20d | %20d |\n", INT_MIN, INT_MAX);
+    printf("| short int              | %20d | %20d |\n", SHRT_MIN, SHRT_MAX);
+    printf("| unsigned int           | %20u | %20u |\n", 0, UINT_MAX);
+    printf("| long int               | %20ld | %20ld |\n", LONG_MIN, LONG_MAX);
+    printf("| unsigned long int      | %20lu | %20lu |\n", 0UL, ULONG_MAX);
+    printf("| long long int          | %20lld | %20lld |\n", LLONG_MIN, LLONG_MAX);
+    printf("| unsigned long long int | %20llu | %20llu |\n", 0ULL, ULLONG_MAX);
 
 }
-// Função de overflow
-void demonstrate_overflow() {
-    unsigned char a = 255; 
-    a += 1; 
-    printf("Valor de 'a' apos exceder o limite: %u (overflow ocorreu)\n", a);
+
+// demonstra o overflow em unsigned char
+void exemplo_overflow() {
+    unsigned char a = 255;
+    a++; // Incrementa a variável 'a' e provoca overflow
+    printf("Valor de 'a' apos overflow: %u\n", a);
 }
-//  decifrar a mensagem hexadecimal
-void decrypt_message(int b, char *ciphered) {
-    int len = strlen(ciphered);
+
+// Função que decifra a mensagem hexadecimal
+void decifrar_mensagem(int b, const char *cifrada) {
+    int tamanho = strlen(cifrada);
     printf("Mensagem decifrada: ");
-    
-    for (int i = 0; i < len; i += 2) {
-        char hex[3];
-        hex[0] = ciphered[i];
-        hex[1] = ciphered[i + 1];
-        hex[2] = '\0';
 
-        int decimal_value;
-        sscanf(hex, "%x", &decimal_value);
+    for (int i = 0; i < tamanho; i += 2) {
+        char parte_hex[3] = {cifrada[i], cifrada[i+1], '\0'};
+        int valor_decimal;
+        sscanf(parte_hex, "%x", &valor_decimal);
 
-        int val = func_val(decimal_value, b);
-        if (val != 0) {
-            printf("%c", val);
+        int valor_decifrado = calcular_valor(valor_decimal, b);
+        if (valor_decifrado != 0) {
+            printf("%c", valor_decifrado);
         }
     }
     printf("\n");
 }
 
 int main() {
-    //  Limites das variáveis
-    print_limits();
-    
-    // Exemplo de overflow
-    demonstrate_overflow();
-    
-    // Decifrar a mensagem
-    int test_cases;
-    printf("Digite o numero de casos de teste: ");
-    scanf("%d", &test_cases);
+    //  limites das variáveis
+    exibir_limites();
 
-    for (int i = 0; i < test_cases; i++) {
+    // Demonstra o overflow
+    exemplo_overflow();
+
+    // número de casos de teste
+    int casos_de_teste;
+    printf("Informe o numero de casos de teste: ");
+    scanf("%d", &casos_de_teste);
+
+    // Loop para processar cada caso de teste
+    for (int i = 0; i < casos_de_teste; i++) {
         int b;
-        char ciphered[100];
+        char mensagem_cifrada[100];
 
-        printf("Digite o valor de b: ");
+        // Lê o valor de 'b' e a mensagem cifrada em hexadecimal
+        printf("Informe o valor de b: ");
         scanf("%d", &b);
 
         printf("Digite a mensagem cifrada (em hexadecimal): ");
-        scanf("%s", ciphered);
+        scanf("%s", mensagem_cifrada);
 
-        decrypt_message(b, ciphered);
+        decifrar_mensagem(b, mensagem_cifrada);
     }
+
     return 0;
 }
